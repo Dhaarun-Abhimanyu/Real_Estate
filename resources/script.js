9eff86a881ceeee6874ec58281b9a429c5663907
Dhaarun-Abhimanyu <147942802+Dhaarun-Abhimanyu@users.noreply.github.com>
2023-10-15 11:44:36 +0530

    Delete resources directory

--- resources/script.js
+++
@@ -1,6 +0,0 @@
-const browse = document.getElementById('browse');
-const subpage = document.getElementById('subpage');
-
-showSubpageButton.addEventListener('click', function () {
-    subpage.classList.toggle('hidden');
-});
